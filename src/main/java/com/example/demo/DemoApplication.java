056d45555733861fe045f3d2b20edce077fc6395
zhong <zhongfangwei@163.com>
2020-04-13 10:33:07 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.PropertySources;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("项目启动");
         SpringApplication.run(DemoApplication.class, args);
     }
 

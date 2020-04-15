8d752f5aa8a6110f2fe65863b42f2346254ea12f
zhong <zhongfangwei@163.com>
2020-04-15 13:46:54 +0800

    数据库user表

---
+++ src/main/resources/user.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(20) NOT NULL AUTO_INCREMENT COMMENT '唯一标示',
+  `code` varchar(20) DEFAULT NULL COMMENT '编码',
+  `name` varchar(64) DEFAULT NULL COMMENT '名称',
+  `status` char(1) DEFAULT '1' COMMENT '状态 1启用 0 停用',
+  `gmt_create` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
